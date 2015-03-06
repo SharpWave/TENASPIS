9a319be4f65a4a9aff34be750e1db4b21eae31ca
SharpWave <sullivan.dw@gmail.com>
2015-03-06 13:47:55 -0500

    the smoother multiplies values by 100 to help get around the problems with smoothing

--- TempSmoothMovie.m
+++ TempSmoothMovie.m
@@ -12,13 +12,13 @@ smfun = hann(smoothfr)./sum(hann(smoothfr));
 h5create(outfile,'/Object',info.Dataspace.Size,'ChunkSize',[XDim YDim 1 1],'Datatype','int32');
 
 for i = 1:smoothfr-1
-    F{i} = int32(h5read(infile,'/Object',[1 1 i 1],[XDim YDim 1 1]));
+    F{i} = int32(h5read(infile,'/Object',[1 1 i 1],[XDim YDim 1 1])).*100;
     h5write(outfile,'/Object',int32(F{i}),[1 1 i 1],[XDim YDim 1 1]);
 end
 
 for i = smoothfr:NumFrames
   display(['Smoothing movie frame ',int2str(i),' out of ',int2str(NumFrames)]);
-  F{smoothfr} = int32(h5read(infile,'/Object',[1 1 i 1],[XDim YDim 1 1]));
+  F{smoothfr} = int32(h5read(infile,'/Object',[1 1 i 1],[XDim YDim 1 1])).*100;
   Fout = zeros(size(F{1}));
   for j = 1:smoothfr
     Fout = Fout+double(F{j}).*smfun(j);
