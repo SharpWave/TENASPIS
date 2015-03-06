eca9feee37dcaf75c3f83ac48f4ac83cc8dd9dc7
SharpWave <sullivan.dw@gmail.com>
2015-03-06 13:30:14 -0500

    got rid of stats from within changemovie

--- ChangeMovie.m
+++ ChangeMovie.m
@@ -1,4 +1,4 @@
-function [stdframe,meanframe] = ChangeMovie(infile,outfile);
+function [] = ChangeMovie(infile,outfile);
 
 
 
@@ -10,15 +10,13 @@ YDim = info.Dataspace.Size(2);
 h5create(outfile,'/Object',info.Dataspace.Size,'ChunkSize',[XDim YDim 1 1],'Datatype','int32');
 
 F0 = h5read(infile,'/Object',[1 1 1 1],[XDim YDim 1 1]);
-h5write(outfile,'/Object',double(zeros(size(F0))),[1 1 1 1],[XDim YDim 1 1]);
+h5write(outfile,'/Object',int32(zeros(size(F0))),[1 1 1 1],[XDim YDim 1 1]);
 
 for i = 2:NumFrames
   display(['Calculating temporal difference for movie frame ',int2str(i),' out of ',int2str(NumFrames)]);
   F1 = h5read(infile,'/Object',[1 1 i 1],[XDim YDim 1 1]);
   DF = F1-F0;
   F0 = F1;
-  stdframe(i) = std(DF(:));
-  meanframe(i) = mean(DF(:));
   h5write(outfile,'/Object',int32(DF),[1 1 i 1],[XDim YDim 1 1]);
 end
 
