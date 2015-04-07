7c530af3df8b6b2eacda393ad6b46c255e8b1959
SharpWave <sullivan.dw@gmail.com>
2015-04-07 16:11:49 -0400

    New PFstats function for looking at placefield sizes, locations, and visits

--- PFstats.m
+++ PFstats.m
@@ -11,6 +11,9 @@ load PlaceMaps.mat; % x y t xOutline yOutline speed minspeed FT TMap RunOccMap O
 
 Min_pT = 0.02; % minimum probability of transient
 NumNeurons = length(NeuronImage);
+NumFrames = length(Xbin);
+
+% some analysis using bwconncomp and regionprops
 
 for i = 1:NumNeurons
     ThreshMap = TMap{i}.*(TMap{i} > Min_pT);
@@ -21,6 +24,31 @@ end
 
 for i = 1:NumNeurons
     NumPF(i) = b{i}.NumObjects;
+    for j = 1:NumPF(i)
+        PFpixels{i,j} = b{i}.PixelIdxList{j};
+        PFsize(i,j) = r{i}(j).Area;
+        PFcentroid{i,j} = r{i}(j).Centroid;
+    end
+    [~,MaxPF(i)] = 
+end
+
+% for every place field, figure out how many times the mouse passed through
+% it
+
+% convert Xbin and Ybin into a single number denoting where the mouse is
+loc_index = sub2ind(size(TMap{1}),Xbin,Ybin); % Y and X purposely switched.  I 
+
+for i = 1:NumNeurons
+    for j = 1:NumPF(i)
+        PixelBool = zeros(1,NumFrames);
+        for k = 1:NumFrames
+            PixelBool(k) = ismember(loc_index(k),PFpixels{i,j});
+        end
+        PFepochs{i,j} = NP_FindSupraThresholdEpochs(PixelBool,eps,0);
+    end
+end
+keyboard;
+
     
     
 
