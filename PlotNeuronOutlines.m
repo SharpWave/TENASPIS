2e965a1dd575dcc510a0d984b154183e12e134f9
SharpWave <sullivan.dw@gmail.com>
2015-04-22 23:41:39 -0400

    random offsets

--- PlotNeuronOutlines.m
+++ PlotNeuronOutlines.m
@@ -19,7 +19,9 @@ for i = 1:length(clusterlist)
     temp(PixelList{i}) = 1;
     b = bwboundaries(temp);
     x{i} = b{1}(:,1);
+    x{i} = x{i}+(rand(size(x{i}))-0.5)/2;
     y{i} = b{1}(:,2);
+    y{i} = y{i}+(rand(size(y{i}))-0.5)/2;
     plot(y{i},x{i},'Color',colors(clusterlist(i),:));hold on;
 end
 hold off;
