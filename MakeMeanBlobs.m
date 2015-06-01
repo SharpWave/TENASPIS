6273549520d823ba5e1a07be02e8b6af3a3d52b0
SharpWave <sullivan.dw@gmail.com>
2015-06-01 15:35:37 -0400

    added plotting

--- MakeMeanBlobs.m
+++ MakeMeanBlobs.m
@@ -27,8 +27,28 @@ for i = 1:length(c)
         MeanBlobs{CurrNeuron} = MeanBlobs{CurrNeuron} + tempFrame;
         NumFrames(CurrNeuron) = NumFrames(CurrNeuron) + 1;
     end
-
     
-
+    
+    
 end
+
+colors = rand(length(MeanBlobs),3);
+
+for i = 1:length(MeanBlobs)
+    
+    temp = zeros(Xdim,Ydim);
+    try
+        temp(find(MeanBlobs{i} > 0.9)) = 1;
+    catch
+        continue;
+    end
+    
+    b = bwboundaries(temp);
+    x{i} = b{1}(:,1);
+    x{i} = x{i}+(rand(size(x{i}))-0.5)/2;
+    y{i} = b{1}(:,2);
+    y{i} = y{i}+(rand(size(y{i}))-0.5)/2;
+    plot(y{i},x{i},'Color',colors(i,:));hold on;
+end
+hold off;
 keyboard;
