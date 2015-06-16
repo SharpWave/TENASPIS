76999e5d943ee4a9b9aabaad2732679595ed55e6
nkinsky <kinsky@bu.edu>
2015-06-16 08:23:01 -0400

    Not meaningful - for testing out multi_mask_reg for G30

--- GetInitRegMaskInfo.m
+++ GetInitRegMaskInfo.m
@@ -6,6 +6,11 @@ AI(1).animal = 'GCaMP6f_31';
 AI(1).init_date = '09_29_2014';
 AI(1).init_sess = 1;
 
+AI(2).animal = 'GCamp6f_30';
+AI(2).init_date = '11_11_2014';
+AI(2).init_sess = 1;
+
+
 for i = 1:length(AI)
     if (strcmp(AI(i).animal,animal_id))
         init_date = AI(i).init_date;
