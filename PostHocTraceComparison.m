18e6e2e94c99ffb58869c228c7f9541f1497e36f
SharpWave <sullivan.dw@gmail.com>
2015-06-26 13:03:20 -0400

    new script for looking at Tenaspis output

---
+++ PostHocTraceComparison.m
@@ -0,0 +1,12 @@
+function [ output_args ] = PostHocTraceComparison()
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+% load the stuff
+
+% for each neuron:
+
+
+
+end
+
