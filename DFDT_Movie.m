01f81d4ac71c48105836c039782b5e9472c22309
SharpWave <sullivan.dw@gmail.com>
2015-10-10 13:33:48 -0400

    fixes

--- DFDT_Movie.m
+++ DFDT_Movie.m
@@ -1,5 +1,5 @@
 function [] = DFDT_Movie(infile,outfile);
-
+% [] = DFDT_Movie(infile,outfile);
 
 
 info = h5info(infile,'/Object');
@@ -7,19 +7,25 @@ NumFrames = info.Dataspace.Size(3);
 XDim = info.Dataspace.Size(1);
 YDim = info.Dataspace.Size(2);
 
-h5create(outfile,'/Object',info.Dataspace.Size,'ChunkSize',[XDim YDim 1 1],'Datatype','int16');
+h5create(outfile,'/Object',info.Dataspace.Size,'Datatype','int16');
 
 F0 = h5read(infile,'/Object',[1 1 1 1],[XDim YDim 1 1]);
+%F0 = int16(F0);
 h5write(outfile,'/Object',int16(zeros(size(F0))),[1 1 1 1],[XDim YDim 1 1]);
 
 for i = 2:NumFrames
   display(['Calculating temporal difference for movie frame ',int2str(i),' out of ',int2str(NumFrames)]);
-  F1 = h5read(infile,'/Object',[1 1 i 1],[XDim YDim 1 1]);
+  F1 = h5read(infile,'/Object',[1 1 i 1],[XDim YDim 1 1])*500;
   DF = F1-F0;
   F0 = F1;
+
   if (i <= 20)
-      DF = zeros(size(DF));
+      DF = (zeros(size(DF)));
   end
+  if ((max(DF(:)) > intmax('int16')) | (min(DF(:)) < intmin('int16')))
+      error('integer conversion error');
+  end
+  
   h5write(outfile,'/Object',int16(DF),[1 1 i 1],[XDim YDim 1 1]);
 end
 
