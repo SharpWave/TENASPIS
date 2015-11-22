9ef5b6fa1e486098d4a9cf60f2f566c3251a4c72
wmau <wmau@bu.edu>
2015-11-22 16:39:24 -0500

    Revert "Revert "Merge remote-tracking branch 'origin/master'""
    This reverts commit b3ddeebbcf5b1eb413e4043bdf3d05c5f9c8d7a1.

--- Tenaspis.m
+++ Tenaspis.m
@@ -36,6 +36,9 @@ function [] = Tenaspis(infile,varargin)
 % 'sess_date': the date the session took place, e.g., '09_29_2014'
 % 'sess_num': which session for a given date to analyze
 %
+%   'min_trans_length': see MakeTransients, minimum length a transient must
+%   last to be considered, optional and if left blank will default to 5
+%
 % ----------------
 %
 % EXAMPLE: Tenaspis('Obj1 - ICsm.h5','animal_id','GCaMP6f_31','sess_date','10_17_2014','sess_num',1,'no_movie_process',1)
@@ -47,6 +50,14 @@ function [] = Tenaspis(infile,varargin)
 % EXAMPLE: Tenaspis('Obj1 - ICsm.h5','manual_mask',1)
 % this runs Tenaspis the 'old' way with a manually drawn mask
 
+%% Get varargins
+for j = 1:length(varargin)
+   if strcmpi(varargin{j},'min_trans_length')
+       min_trans_length = varargin{j+1};
+   end
+end
+
+%%
 ManMask = 0;
 no_movie_process = 0;
 
@@ -83,7 +94,13 @@ if(~no_movie_process) % don't run these if already run
     TempSmoothMovie(infile,'SMovie.h5',SmoothWindowWidth);
     
     %% Step 2: Take the first derivative
-    DFDT_Movie('SMovie.h5','D1Movie.h5');
+    multiplier_use = DFDT_Movie('SMovie.h5','D1Movie.h5');
+    if ~isempty(multiplier_use)
+        delete D1Movie.h5
+        multiplier_use = DFDT_Movie('SMovie.h5','D1Movie.h5',multiplier_use);
+        save multiplier.mat multiplier_use
+    end
+    delete SMovie.h5
 end
 
 if (~no_blobs)
@@ -106,13 +123,21 @@ if (~no_blobs)
     ExtractBlobs('D1Movie.h5',0,thresh,neuronmask);
 end
 %% Step 6: String Blobs into calcium transients
-MakeTransients('D1Movie.h5');
+if exist('min_trans_length','var')
+    MakeTransients('D1Movie.h5','','min_trans_length',min_trans_length);
+else
+    MakeTransients('D1Movie.h5')
+end
 
 %temporary: needed because of previous changes:
 !del InitClu.mat
 
 %% Step 7: Decide which transients (segments) belong to the same neuron
-MakeNeurons();
+if exist('min_trans_length','var')
+    MakeNeurons('min_trans_length',min_trans_length);
+else
+    MakeNeurons()
+end
 
-
+end
 
---
+++ session_add_dialog.m
@@ -0,0 +1,265 @@
+function varargout = session_add_dialog(varargin)
+% SESSION_ADD_DIALOG MATLAB code for session_add_dialog.fig
+%      SESSION_ADD_DIALOG, by itself, creates a new SESSION_ADD_DIALOG or raises the existing
+%      singleton*.
+%
+%      H = SESSION_ADD_DIALOG returns the handle to a new SESSION_ADD_DIALOG or the handle to
+%      the existing singleton*.
+%
+%      SESSION_ADD_DIALOG('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in SESSION_ADD_DIALOG.M with the given input arguments.
+%
+%      SESSION_ADD_DIALOG('Property','Value',...) creates a new SESSION_ADD_DIALOG or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before session_add_dialog_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to session_add_dialog_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help session_add_dialog
+
+% Last Modified by GUIDE v2.5 06-Nov-2015 14:38:20
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @session_add_dialog_OpeningFcn, ...
+                   'gui_OutputFcn',  @session_add_dialog_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before session_add_dialog is made visible.
+function session_add_dialog_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to session_add_dialog (see VARARGIN)
+
+% Choose default command line output for session_add_dialog
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes session_add_dialog wait for user response (see UIRESUME)
+% uiwait(handles.session_adder);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = session_add_dialog_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in movie_select_button.
+function movie_select_button_Callback(hObject, eventdata, handles)
+% hObject    handle to movie_select_button (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+[FileName,PathName] = uigetfile('*.h5','Pick an .h5 movie file')
+handles.filname = FileName;
+handles.pathname = PathName;
+guidata(hObject,handles);
+
+% --- Executes on selection change in popupmenu1.
+function popupmenu1_Callback(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns popupmenu1 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from popupmenu1
+
+
+% --- Executes during object creation, after setting all properties.
+function popupmenu1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in date_select_button.
+function date_select_button_Callback(hObject, eventdata, handles)
+% hObject    handle to date_select_button (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+ymd = uisetdate2;
+handles.date = [int2str(ymd(2)),'/',int2str(ymd(3)),'/',int2str(ymd(1))];
+guidata(hObject,handles);
+
+% --- Executes on selection change in session_number_menu.
+function session_number_menu_Callback(hObject, eventdata, handles)
+% hObject    handle to session_number_menu (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns session_number_menu contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from session_number_menu
+
+
+% --- Executes during object creation, after setting all properties.
+function session_number_menu_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to session_number_menu (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function animal_name_box_Callback(hObject, eventdata, handles)
+% hObject    handle to animal_name_box (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of animal_name_box as text
+%        str2double(get(hObject,'String')) returns contents of animal_name_box as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function animal_name_box_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to animal_name_box (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function experiment_type_box_Callback(hObject, eventdata, handles)
+% hObject    handle to experiment_type_box (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of experiment_type_box as text
+%        str2double(get(hObject,'String')) returns contents of experiment_type_box as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function experiment_type_box_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to experiment_type_box (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in OKbutton.
+function OKbutton_Callback(hObject, eventdata, handles)
+% hObject    handle to OKbutton (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% grab data from popup and edit text fields
+newdata.Animal = handles.animal_name_box.String;
+newdata.Date = handles.date;
+newdata.Session = str2num(handles.session_number_menu.String(handles.session_number_menu.Value));
+newdata.Env = handles.experiment_type_box.String;
+newdata.Room = handles.room_box.String;
+newdata.Location = [handles.filepath,handles.filename];
+newdata.Notes = [];
+
+ht = findobj('Tag','tenaspisgui');
+dt = guidata(ht);
+
+dt.MD(end+1) = newdata;
+guidata(ht,dt);
+
+
+h = findobj('Tag','session_adder');
+close(h);
+
+
+% if data is invalid or fields are left blank, show dialog and return
+
+% show user the entry in a dialog with yes/no buttons, 
+%   if user clicks yes add entry to database, close yes/no dialog, close
+%   session adder
+%   if user clicks no, cloes yes/no dialog
+% add the entry to 
+
+
+
+function room_box_Callback(hObject, eventdata, handles)
+% hObject    handle to room_box (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of room_box as text
+%        str2double(get(hObject,'String')) returns contents of room_box as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function room_box_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to room_box (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit5_Callback(hObject, eventdata, handles)
+% hObject    handle to edit5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit5 as text
+%        str2double(get(hObject,'String')) returns contents of edit5 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit5_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
