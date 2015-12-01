ae5587d8e35b403b280d5c7b2ccd00b901adacb5
wmau <wmau@bu.edu>
2015-12-01 11:07:52 -0500

    Revert "Added G45 treadmill to GetInitRegMaskInfo"
    This reverts commit a7dc2a0e22b8907547096e2a91a584f152375a37.

--- GetInitRegMaskInfo.m
+++ GetInitRegMaskInfo.m
@@ -47,10 +47,6 @@ AI(7).animal = 'GCamp6f_30_2';
 AI(7).init_date = '06_10_2015';
 AI(7).init_sess = 1;
 
-AI(8).animal = 'GCamp6f_45_treadmill';
-AI(8).init_date = '11_02_2015';
-AI(8).init_sess = 1; 
-
 for i = 1:length(AI)
     if (strcmpi(AI(i).animal,animal_id))
         init_date = AI(i).init_date;
