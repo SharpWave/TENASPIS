de4c15fbc5e8294c3b04182872eba1a2668c27c7
SharpWave <sullivan.dw@gmail.com>
2015-12-14 15:05:28 -0500

    added # transients output

--- FTtoTrRate.m
+++ FTtoTrRate.m
@@ -1,4 +1,4 @@
-function [CaTrRate] = FTtoTrRate(FT,sr)
+function [CaTrRate,NumTr] = FTtoTrRate(FT,sr)
 % Takes a binary neuron activation matrix(NumNeurons by NumFrames) and
 % converts this to Transients per second by convolving the beginning of the
 % events with a 1-second window.
@@ -9,8 +9,14 @@ end
 NumNeurons = size(FT,1);
 NumFrames = size(FT,2);
 
+CaTrRate = zeros(size(FT));
+
 for i = 1:NumNeurons
     temp = NP_FindSupraThresholdEpochs(FT(i,:),eps,0);
+    NumTr(i) = size(temp,1);
+    if (isempty(temp))
+        continue;
+    end
     TrStarts = temp(:,1);
     TrBool = zeros(NumFrames,1);
     TrBool(TrStarts) = 1;
