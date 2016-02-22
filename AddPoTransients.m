9c115733912761085f9a447163684552085c68a3
SharpWave <sullivan.dw@gmail.com>
2016-02-22 13:53:25 -0500

    finds and adds potential transients

---
+++ AddPoTransients.m
@@ -0,0 +1,57 @@
+function [ output_args ] = AddPoTransients()
+%UNTITLED5 Summary of this function goes here
+%   Detailed explanation goes here
+load pPeak.mat;
+load ExpTransients.mat;
+load('ProcOut.mat','NumNeurons','NumFrames','NeuronPixels');
+
+expPosTr = PosTr;
+
+display('checking buddies');
+for j = 1:NumNeurons
+    buddies{j} = [];
+    for i = 1:NumNeurons
+        if (i == j)
+            continue;
+        end
+        
+        if (length(intersect(NeuronPixels{i},NeuronPixels{j})) > 0)
+            buddies{j} = [buddies{j},i];
+        end
+    end
+end
+
+for i = 1:NumNeurons
+    display(['Neuron ',int2str(i)]);
+    PoEpochs = NP_FindSupraThresholdEpochs(PoPosTr(i,:),eps);
+    for j = 1:size(PoEpochs,1)
+        % check for buddies
+        buddyspike = 0;
+        for k = 1:length(buddies{i})
+            if (sum(expPosTr(buddies{i}(k),PoEpochs(j,1):PoEpochs(j,2))) > 0)
+                buddyspike = 1;
+            end
+        end
+        
+        if (buddyspike)
+            display('buddy spike');
+            continue;
+        end
+        
+        % no buddy spike, check peak
+        f = loadframe('DFF.h5',PoTrPeakIdx{i}(j));
+        [~,maxidx] = max(f(NeuronPixels{i}));
+        peakpeak = pPeak{i}(maxidx);
+        peakrank = mRank{i}(maxidx);
+        if ((peakpeak > 0) & (peakrank > 0.7))
+            display('new transient!');
+            expPosTr(i,PoEpochs(j,1):PoEpochs(j,2)) = 1;
+        else
+            display('pixels off kilter');
+        end
+        
+    end
+end
+    
+    save expPosTr.mat expPosTr;
+    
--- ExpandTransients.m
+++ ExpandTransients.m
@@ -51,12 +51,56 @@ end
 
 
 % find potential missed transients
-PoPosTr = zeros(NumNeurons,NumFrames);
+PrePoPosTr = zeros(NumNeurons,NumFrames);
 for i = 1:NumNeurons
     tr = trace(i,:);
-    PoPosTr(i,1:NumFrames) = (tr >= MinPeak(i)).*(PosTr(i,:) == 0);
+    PrePoPosTr(i,1:NumFrames) = (tr >= MinPeak(i)).*(PosTr(i,:) == 0);
 end
- save ExpTransients.mat MaxPeak MinPeak PosTr PoPosTr;   
+
+for i = 1:NumNeurons
+    i
+    tr = trace(i,:);
+    PoPosTr(i,1:NumFrames) = 0;
+    activefr = find(PrePoPosTr(i,:));
+    PoNumTr(i) = 0;
+    for j = 1:length(activefr)
+        if (PoPosTr(i,activefr(j)))
+            continue;
+        end
+        
+        PoNumTr(i) = PoNumTr(i) + 1;
+        
+        % find backward extent
+        curr = activefr(j);
+        
+        while ((tr(curr) > 0) & (curr < NumFrames))
+            curr = curr + 1;
+        end
+        
+        TrEnd = max(curr - 1,activefr(j));
+        
+        curr = activefr(j);
+        
+        while ((tr(curr) > 0) & (curr > 1))
+            curr = curr - 1;
+        end
+        
+        TrStart = min(curr + 1,activefr(j));
+        
+        PoPosTr(i,TrStart:TrEnd) = 1;
+        
+        % stats
+
+        PoTrLength{i}(PoNumTr(i)) = TrEnd-TrStart+1;
+        [PoTrPeakVal{i}(PoNumTr(i)),idx] = max(tr(TrStart:TrEnd));
+        PoTrPeakIdx{i}(PoNumTr(i)) = idx+TrStart-1;
+    end
+    PoMinPeak(i) = min(TrPeakVal{i});
+    PoMaxPeak(i) = max(TrPeakVal{i});
+    PoAvgLength(i) = mean(TrLength{i});
+end
+
+ save ExpTransients.mat MaxPeak MinPeak PosTr PoPosTr PrePoPosTr PoTrPeakIdx PoNumTr;   
 if (Todebug)
     for i = 1:NumNeurons
         plot(FT(i,:)*5);hold on;plot(PosTr(i,:)*5);plot(trace(i,:));plot(zscore(difftrace(i,:)));plot(PoPosTr(i,:),'-r','LineWidth',2);hold off;set(gca,'YLim',[-10 10]);pause;
