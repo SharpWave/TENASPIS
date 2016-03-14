acfac55356c4c88f08977ccea477c90e47c0475b
SharpWave <sullivan.dw@gmail.com>
2016-03-14 12:55:42 -0400

    code cleanup and improvements

--- SegmentFrame.m
+++ SegmentFrame.m
@@ -1,5 +1,5 @@
-function [cc,PeakPix] = SegmentFrame(frame,toplot,mask,thresh)
-% [frame,cc,ccprops] = SegmentFrame(frame,toplot,mask,thresh)
+function [cc,PeakPix] = SegmentFrame(frame,mask,thresh)
+% [frame,cc,ccprops] = SegmentFrame(frame,mask,thresh)
 % Copyright 2015 by David Sullivan and Nathaniel Kinsky
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % This file is part of Tenaspis.
@@ -17,91 +17,71 @@ function [cc,PeakPix] = SegmentFrame(frame,toplot,mask,thresh)
 %     You should have received a copy of the GNU General Public License
 %     along with Tenaspis.  If not, see <http://www.gnu.org/licenses/>.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
 
-if (nargin < 2)
-    toplot = 0;
-end
+% Parameters
+minpixels = 80; % minimum blob size during initial segmentation
+adjminpixels = 50; % minimum blob size during re-segmentation attempts
+threshinc = 0.02; % how much to increase threshold by on each re-segmentation iteration
+neuronthresh = 150; % maximum blob size to be considered a neuron
+minsolid = 0.85; % minimum blob solidity to be considered a neuron
 
-minpixels = 80;%80
-adjminpixels = 50;
-numpan = 3;
-threshinc = 0.02;%10
-neuronthresh = 150;%300
-minsolid = 0.85;
+
 PeakPix = [];
-
-initframe = double(frame);
-minval = min(initframe(:));
-
-if (toplot)
-    figure;
-    subplot(1,numpan,1);imagesc(frame); title ('raw input');colormap(gray);
-end
-
 badpix = find(mask == 0);
 
+% threshold and segment the frame
+initframe = double(frame);
+minval = min(initframe(:));
 threshframe = frame > thresh;
-
-threshframe = bwareaopen(threshframe,minpixels,4); % remove smaller than minpixels
-
-if (toplot)
-    subplot(1,numpan,2);imagesc(threshframe); title('input to bwconncomp');
-end
-
+threshframe = bwareaopen(threshframe,minpixels,4); % remove blobs smaller than minpixels
 cc = bwconncomp(threshframe,4);
 rp = regionprops(cc,'Area','Solidity');
 
+% exit if no blobs found
 if (isempty(cc.PixelIdxList))
-    frame = zeros(cc.ImageSize(1),cc.ImageSize(2));
     PeakPix = [];
-    display('hit this');
+    display('no blobs detected');
     return;
 end
 
-
-% ok, now sort the cc's by their sizes
+% get area and solidity info
 for i = 1:length(cc.PixelIdxList)
     segsize(i) = rp(i).Area;
     segsolid(i) = rp(i).Solidity;
 end
 
+% the cc's in CCgoodidx satisfy the size and solidity criteria
 CCgoodidx = intersect(find(segsize <= neuronthresh),find(segsolid >= minsolid));
+
+% the cc's in CCquestionidx do not satisfy the criteria (but might if we
+% raise the threshold!)
 CCquestionidx = union(find(segsize > neuronthresh),find(segsolid < minsolid));
 
-% the cc's in CCquestionidx might be multiple cells
-if (toplot)
-    figure
-end
 newlist = [];
 currnewList = 0;
+
 for i = 1:length(CCquestionidx)
-    % we want to try to increase the threshold and do a bwconncomp on only
-    % the pixels that were part of this cc
-    % if this creates any cc's that are below the neuron size threshold, we eliminate those pixels and continue to raise the threshold
-    % repeating this until all pixels have been eliminated or there are no
-    % more cc's
+    % make a frame containing only the pixels in this blob (all else set to
+    % minval)
     qidx = CCquestionidx(i);
     temp = zeros(cc.ImageSize(1),cc.ImageSize(2))+minval;
     temp(cc.PixelIdxList{qidx}) = initframe(cc.PixelIdxList{qidx});
+    
+    % increase threshold one increment from baseline
     tempthresh = thresh + threshinc;
-    keepgoing = 1;
-    while(keepgoing)
-        keepgoing = 0;
+    BlobsInFrame = 1;
+    
+    while(BlobsInFrame)
+        BlobsInFrame = 0;
+        % threshold and segment the frame
         threshframe = temp > tempthresh;
         threshframe = bwareaopen(threshframe,adjminpixels,4);
-        if (toplot)
-            subplot(1,2,1);imagesc(threshframe);colormap gray;caxis([0 1]);
-            subplot(1,2,2);imagesc(temp);caxis([tempthresh max(temp(:))]);pause;
-        end
-        
-        
         bb = bwconncomp(threshframe,4);
         rp = regionprops(bb,'Area','Solidity');
+        
         if (~isempty(bb.PixelIdxList))
-            % there were blobs, check if any of them are under
-            % thresh
+            % there were blobs, check if any of them satisfy size and
+            % solidity criteria
             bsize = [];
             bSolid = [];
             
@@ -109,38 +89,32 @@ for i = 1:length(CCquestionidx)
                 bsize(j) = rp(j).Area;
                 bSolid(j) = rp(j).Solidity;
             end
-            %             tempthresh
-            %             bsize
-            %             bSolid
-            
-            %%%TODO also check for ellipsoid border by comparing the size
-            % to the size of the border
             
             newn = intersect(find(bsize <= neuronthresh),find(bSolid >= minsolid));
-            if (~isempty(newn))
-                for j = 1:length(newn)
-                    % this is a new list
-                    currnewList = currnewList + 1;
-                    newlist{currnewList} = bb.PixelIdxList{newn(j)};
-                    %display('successfully found a new neuron');
-                    if (toplot)
-                        pause;
-                    end
-                end
+            
+            for j = 1:length(newn)
+                % append new blob pixel lists
+                currnewList = currnewList + 1;
+                newlist{currnewList} = bb.PixelIdxList{newn(j)};
             end
+            
             
             if (length(newn) == length(bb.PixelIdxList))
                 % nothing left to split
                 break;
             else
-                % still over-threshold blobs left
+                % still blobs left
+                BlobsInFrame = 1;
                 oldn = union(find(bsize > neuronthresh), find(bSolid<minsolid));
+                
+                % make a frame containing the remaining blobs
                 temp = zeros(cc.ImageSize(1),cc.ImageSize(2))+ minval;
                 for j = 1:length(oldn)
                     temp(bb.PixelIdxList{oldn(j)}) = initframe(bb.PixelIdxList{oldn(j)});
                 end
+                
+                % increase threshold
                 tempthresh = tempthresh + threshinc;
-                keepgoing = 1;
                 continue;
             end
         else
@@ -150,11 +124,9 @@ for i = 1:length(CCquestionidx)
         end
     end
 end
-close all;
 
 numlists = 0;
 newcc.PixelIdxList = [];
-
 
 for i = 1:length(CCgoodidx)
     if (isempty(intersect(cc.PixelIdxList{CCgoodidx(i)},badpix)))
