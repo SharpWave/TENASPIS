d27d44d31b1e56e3d40b751e9348e7e65cdff07d
nkinsky <kinsky@bu.edu>
2016-04-05 09:33:20 -0400

    Small edits - not ready for merging yet.

--- Tenaspis2.m
+++ Tenaspis2.m
@@ -6,7 +6,7 @@ function Tenaspis2(animal_id,sess_date,sess_num)
 MasterDirectory = 'C:\MasterData';
 
 [init_date,init_sess] = GetInitRegMaskInfo(animal_id);
-% init_dir = ChangeDirectory(animal_id,init_date,init_sess);
+init_dir = ChangeDirectory(animal_id,init_date,init_sess);
 % init_tif = fullfile(init_dir,'ICmovie_min_proj.tif');
 
 init_mask_loc = fullfile(MasterDirectory,[animal_id,'_initialmask.mat']);
@@ -15,7 +15,10 @@ reg_struct.Animal = animal_id;
 reg_struct.Date = sess_date;
 reg_struct.Session = sess_num;
 
-mask_multi_image_reg(init_mask_loc,init_date,init_sess,reg_struct);
+sess_dir = ChangeDirectory(animal_id,sess_date,sess_num); % Change to session directory
+if ~exist('mask_reg.mat','file')
+    mask_multi_image_reg(init_mask_loc,init_date,init_sess,reg_struct);
+end
 
 load mask_reg
 mask_reg = logical(mask_reg); 
@@ -30,26 +33,29 @@ MakeTransients('DFF.h5',0); % Dave - the inputs to this are currently unused
 !del InitClu.mat
 
 %% Group together individual transients under individual neurons
+
 disp('Making neurons...'); 
 MakeNeurons('min_trans_length',10);
 
-%% Pull traces out of each neuron using the High-pass movie
+% Pull traces out of each neuron using the High-pass movie
 disp('Normalizing traces...'); 
 NormalTraces('SLPDF.h5');
 
-%% Expand transients
+% Expand transients
 disp('Expanding transients...'); 
 ExpandTransients(0);
 
-%% Calculate peak of all transients
+% Calculate peak of all transients
 disp('Calculating pPeak...'); 
 Calc_pPeak;
-
+%%
 AddPoTransients;
 
-%% Determine rising events/on-times for all transients
+% Determine rising events/on-times for all transients
 disp('Finalizing...');
 DetectGoodSlopes;
+
+profile viewer
 
 %% Calculate place fields and accompanying statistics
 CalculatePlacefields('201b','alt_inputs','T2output.mat','man_savename',...
