59277b1d3f6919cf5211fbfd6c8403c196241cb9
nkinsky <kinsky@bu.edu>
2016-05-03 16:51:00 -0400

    nada

--- GetInitRegMaskInfo.m
+++ GetInitRegMaskInfo.m
@@ -29,7 +29,7 @@ AI(2).init_sess = 1;
 
 AI(3).animal = 'GCamp6f_44';
 AI(3).init_date = '07_10_2015';
-AI(3).init_sess = 2;
+AI(3).init_sess = 2; 
 
 AI(4).animal = 'GCamp6f_45';
 AI(4).init_date = '08_05_2015';
