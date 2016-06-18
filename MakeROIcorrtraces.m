9cfc6fdb6234fc0cbdac266e3f7a035579dfcfeb
SharpWave <sullivan.dw@gmail.com>
2016-06-18 10:29:21 -0400

    scripts for looking at ROI pixel correlations

--- MakeROIcorrtraces.m
+++ MakeROIcorrtraces.m
@@ -1,20 +1,18 @@
-function [] = MakeROIcorrtraces()
-load ProcOut.mat;
-load ROIavg.mat;
+function [] = MakeROIcorrtraces(NeuronPixels,Xdim,Ydim,NumFrames,ROIavg)
 
 % Initialize progress bar
 p=ProgressBar(NumFrames);
-disp('Calculating traces for each neuron')
-for i = 1:NumFrames
+disp('Calculating traces for each neuron');
+
+
+parfor i = 1:NumFrames
     
     % Read in each frame
     tempFrame = h5read('SLPDF.h5','/Object',[1 1 i 1],[Xdim Ydim 1 1]);
     tempFrame = tempFrame(:);
- 
+    CorrTrace(:,i) = tracecorr(tempFrame,ROIavg,NeuronPixels);
     % Sum up the number of pixels active in each frame for each neuron
-    for j = 1:NumNeurons
-        CorrTrace(j,i) = corr(tempFrame(NeuronPixels{j}),ROIavg{j}(NeuronPixels{j}));
-    end
+
     p.progress; % Update progress bar
 end
 p.stop;
