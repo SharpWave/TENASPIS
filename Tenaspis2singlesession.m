3ea42475dc5cdcff6ca6ccc37231c6837c539a93
SharpWave <sullivan.dw@gmail.com>
2016-06-19 15:03:01 -0400

    changes regarding trace interpretation functions

--- Tenaspis2singlesession.m
+++ Tenaspis2singlesession.m
@@ -5,29 +5,29 @@ function Tenaspis2singlesession()
 %% Extract Blobs
 load singlesessionmask.mat;
 disp('Extracting blobs...'); 
-ExtractBlobs('SLPDF.h5',neuronmask);
+%ExtractBlobs('SLPDF.h5',neuronmask);
 
 %% Connect blobs into transients
 disp('Making transients...');
-MakeTransients; 
+%MakeTransients; 
 !del InitClu.mat
 
 %% Group together individual transients under individual neurons
 disp('Making neurons...'); 
-MakeNeurons('min_trans_length',10);
+%MakeNeurons('min_trans_length',10);
 
 %% Pull traces out of each neuron using the High-pass movie
 disp('Normalizing traces...'); 
 NormalTraces('SLPDF.h5');
-
+MakeROIavg;
+load ProcOut.mat;
+load ROIavg.mat;
+MakeROIcorrtraces(NeuronPixels,Xdim,Ydim,NumFrames,ROIavg);
 %% Expand transients
 disp('Expanding transients...'); 
 ExpandTransients(0);
 
 %% Calculate peak of all transients
-disp('Calculating pPeak...'); 
-Calc_pPeak;
-
 AddPoTransients;
 
 %% Determine rising events/on-times for all transients
