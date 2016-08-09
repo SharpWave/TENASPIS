575091cdff72a9c78e98d5ef26e10edfb5620f19
wmau <wmau@bu.edu>
2016-08-09 13:43:23 -0400

    Helpful commenting.

--- MakeT2Movies.m
+++ MakeT2Movies.m
@@ -1,17 +1,37 @@
 function MakeT2Movies(varargin)
-% MakeT2Movies
+% MakeT2Movies(NAME,VALUE)
+%
+% Tenaspis: Technique for Extracting Neuronal Activity from Single Photon Image Sequences 
+% Copyright 2015 by David Sullivan and Nathaniel Kinsky
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This file is part of Tenaspis.
+% 
+%     Tenaspis is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     Tenaspis is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with Tenaspis.  If not, see <http://www.gnu.org/licenses/>.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 %   Takes cropped, motion-corrected movie and makes two movies from it. 
 %
 % INPUTS
-%   varargin:
-%       'path': string, path containing session usually in the form
-%       X:\Animal\Date\Session. Default=uigetfile. 
+%   NAME:
+%       'path' whose VALUE can be a string, the path containing the session
+%       usually in the form X:\Animal\Date\Session. Default=runs uigetfile.
 %
-%       'd1': logical, whether or not you want to also make a first
-%       derivative movie from the 3-pixel smoothed movie. Default=false. 
+%       'd1' whose VALUE can be a logical, whether or not you want to also
+%       make a first derivative movie from the 3-pixel smoothed movie.
+%       Default=false.
 %   
-% OUTPUTS - saved to directory above MotCorr5
+% OUTPUTS - saved to directory above motion-corrected movie path.
 %
 %   SLPDF.h5: Takes a 3-pixel smoothed version of the input movie and
 %   divides it by the 20-pixel smoothed version of the same movie. Then,
@@ -26,26 +46,30 @@ function MakeT2Movies(varargin)
     p.addParameter('d1',false,@(x) islogical(x));
     p.parse(varargin{:});
     
-    path = p.Results.path;
-    d1 = p.Results.d1; 
+    path = p.Results.path;  %File path.
+    d1 = p.Results.d1;      %Temporal first derivative movie?
     
 %% File names.
+    %If the path is specified, grab the h5 file name.
     if ischar(path)
         cd(fullfile(path,'MotCorrMovie-Objects')); 
         h5 = ls('*.h5'); 
         MotCorrh5 = fullfile(pwd,h5); 
+    %Otherwise, select the file with a UI. 
     else
         [MotCorrh5,path] = uigetfile('*.h5');
         MotCorrh5 = fullfile(path,MotCorrh5);
         path = fileparts(fileparts(path)); % Grab folder above the one containing MotCorrh5 movie
     end
     
-    SLPDFname = fullfile(path,'SLPDF.h5');
-    DFFname = fullfile(path,'DFF.h5'); 
-    threePixName = fullfile(path,'threePixSmooth.h5');
-    tempfilename = fullfile(path,'temp.h5');
+    %File names. 
+    SLPDFname = fullfile(path,'SLPDF.h5');              %Spatial low pass divide DF/F.
+    DFFname = fullfile(path,'DFF.h5');                  %DF/F of spatially smoothed movie.
+    threePixName = fullfile(path,'threePixSmooth.h5');  %Spatially smoothed movie.
+    tempfilename = fullfile(path,'temp.h5');            %Spatial low pass divide (before DF/F).
     
 %% Set up.
+    %Make files. 
     info = h5info(MotCorrh5,'/Object'); 
     [~,Xdim,Ydim,nFrames] = loadframe(MotCorrh5,1);
     h5create(tempfilename,'/Object',info.Dataspace.Size,'ChunkSize',...
@@ -53,7 +77,7 @@ function MakeT2Movies(varargin)
     h5create(threePixName,'/Object',info.Dataspace.Size,'ChunkSize',...
         [Xdim Ydim 1 1],'Datatype','single'); 
     
-    %Filters. 
+    %Spatial filters. 
     LPfilter = fspecial('disk',20);
     threePixfilter = fspecial('disk',3);
 
@@ -106,7 +130,7 @@ function MakeT2Movies(varargin)
     Make_DFF(threePixName,DFFname);
     
     
-%% Delete old files
+%% Delete temporary files
     delete(tempfilename);
     delete(threePixName);
 
