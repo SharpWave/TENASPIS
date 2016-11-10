f41d9483fa31d393d7d52aeb81aff8311d157156
SharpWave <sullivan.dw@gmail.com>
2016-11-10 10:58:30 -0500

    planning for fake movie stuff

---
+++ MakeFakeMovie.m
@@ -0,0 +1,16 @@
+function [ output_args ] = MakeFakeMovie()
+% Makes a fake dataset
+
+%% Set neuron ROIs
+% Set locations
+
+% Set neuron shapes and sizes
+
+% Make fake transient trains
+
+% render the fake dataset
+
+% for i = 1:NumFrames
+
+end
+
