bc1e3bfbd0cc6c853fa18811e27e9c199ccb7d13
SharpWave <sullivan.dw@gmail.com>
2016-11-30 23:00:41 -0500

    new stuff

---
+++ Tenaspis4testing.m
@@ -0,0 +1,32 @@
+function Tenaspis4testing()
+% Quick & dirty Tenaspis2
+% Requires singlesessionmask.mat be present for automated runs
+% use MakeMaskSingleSession if needed
+
+% REQUIREMENT: first call MakeFilteredMovies on your cropped motion-corrected
+% movie
+
+% set global parameter variable
+Set_T_Params('BPDFF.h5');
+
+%% Extract Blobs
+%load singlesessionmask.mat; % if this isn't already present, make it
+
+[Xdim,Ydim] = Get_T_Params('Xdim','Ydim');
+neuronmask = ones(Xdim,Ydim);
+SideBuf = 20;
+neuronmask(1:SideBuf,:) = 0;
+neuronmask(end-SideBuf+1:end,:) = 0;
+neuronmask(:,1:SideBuf) = 0;
+neuronmask(:,end-SideBuf+1:end) = 0;
+ExtractBlobs(neuronmask);
+
+%% Connect blobs into transients
+LinkBlobs();
+RejectBadTransients();
+MakeTransientROIs();
+
+%% Group together individual transients under individual neurons and save data
+MergeTransientROIs;
+InterpretTraces();
+
